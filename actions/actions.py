1514845e2df15be09e5dd07a1130439b3c42eccf
Narayan Bandodker <repomaster@phaidelta.com>
2023-03-28 16:35:16 +0530

    Merge Ashpak's changes (metric nlu)

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,37 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List, Union
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from random import randint
+
+class ActionCheckTemprature(Action):
+    def name(self):
+        return "action_check_temprature"
+    
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain) :
+        return "this is action reply"
+
+class Action_Test(Action):
+    def name(self):
+        return "action_reply_query"
+    
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain) :
+        metric = tracker.get_slot("metric")
+        room = tracker.get_slot("room")
+        aggri = tracker.get_slot("aggregation")
+        if 'temp' in metric:
+            hard_value = f"{randint(20,30)}'C"
+        else:
+            hard_value = f"{randint(60,80)}%"
+        if aggri is None:
+            dispatcher.utter_message(f"{metric} for {room} is {hard_value}")
+        else:
+            dispatcher.utter_message(f"{aggri} {metric} for {room} is {hard_value}")
+        return []
+    
+    # "this is action reply"
+    # {metric} for {room} is {hard_value}
