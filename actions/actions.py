a12a80c097271a0ca281fa9529f7f4569e767d02
Ashpak-phaiDelta <a.shaikh@phaidelta.com>
2023-04-21 20:59:29 +0530

    checked working of forms

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,8 @@ from datetime import datetime, timedelta
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import ActionExecutionRejected
+# from rasa_sdk.forms import FormAction
+from rasa_sdk.forms import FormValidationAction
 
 from .api.aggregation import (
     get_sensor_data,
@@ -159,3 +161,74 @@ class ActionFetchReport(Action):
 
 
         return []
+
+
+class ActionFormInfo(FormValidationAction):
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "form_info"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["NAME", "BRAND"]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+
+        # utter submit template
+        dispatcher.utter_message(template="utter_submit", name=tracker.get_slot('NAME'),
+                                 headset=tracker.get_slot('BRAND'))
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+        print("MAppping slots")
+
+        return {
+            "name": [self.from_entity(entity="NAME", intent='my_name_is'),
+                     self.from_text()],
+            "headset": [self.from_entity(entity="BRAND", intent="headset"),
+                        self.from_text()],
+        }
+
+    @staticmethod
+    def brand_db() -> List[Text]:
+        """Database of supported cuisines"""
+
+        return [
+            "samsung",
+            "One plus",
+            "I-phone",
+        ]
+
+    def validate_brand(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+        print(value)
+        if value.lower() in self.cuisine_db():
+            # validation succeeded, set the value of the "cuisine" slot to value
+            return {"BRAND": value}
+        else:
+            print(value)
+            dispatcher.utter_message(template="utter_wrong_value")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"BRAND": None}
