c5938f637b430a4104b0a3e731c409dd3e91c876
Narayan Bandodker <repomaster@phaidelta.com>
2023-05-29 15:33:13 +0530

    Made chatette multiprocessing Use different branch of chatette that has roles

--- build_chatette_nlu.py
+++ build_chatette_nlu.py
@@ -1,14 +1,31 @@
 
+import multiprocessing
 from pathlib import Path
+from typing import Tuple
 
 from chatette.facade import Facade
+from tqdm import tqdm
 
-def compile_chatette(root : Path):
-    for master_chatette in root.rglob("*master.chatette"):
-        if master_chatette.is_file() and master_chatette.name.split('.')[-1] == 'chatette':
-            save_output_dir = master_chatette.parent.absolute().joinpath("output/")
-            facade = Facade.reset_system(master_chatette, save_output_dir, adapter_str='rasa', force_overwriting=True)
-            facade.run()
+
+def compile_chatette(args: Tuple[Path, Path]):
+    chatette_master_file, output_dir = args
+    facade = Facade.reset_system(chatette_master_file, output_dir, adapter_str='rasa', force_overwriting=True)
+    facade.run()
+
+
+def compile_chatette_dispatch(root: Path):
+    pool = multiprocessing.Pool()
+
+    convert_list = [
+        (master_chatette, master_chatette.parent.absolute().joinpath("output/"))
+        for master_chatette in root.rglob("*master.chatette")
+        if master_chatette.is_file() and master_chatette.name.split('.')[-1] == 'chatette'
+    ]
+
+    list(tqdm(pool.imap_unordered(compile_chatette, convert_list), total=len(convert_list), unit='project'))
+
+    pool.close()
+
 
 if __name__ == "__main__":
-    compile_chatette(Path('./projects/'))
+    compile_chatette_dispatch(Path('./projects/'))
