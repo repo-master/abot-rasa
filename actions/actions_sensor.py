63549afb189c76b4ea3d4a082c17629e9af63894
Narayan Bandodker <repomaster@phaidelta.com>
2023-05-31 20:46:10 +0530

    Docker pip quiet NLU changed stuff to capture entities better Removed unused code

--- actions/actions_sensor.py
+++ actions/actions_sensor.py
@@ -4,12 +4,11 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-import asyncio
 import json
 import logging
 from typing import Any, Dict, List, Optional, Text, Tuple, Union
 
-from rasa_sdk import Action, FormValidationAction, Tracker
+from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.interfaces import Tracker
@@ -19,9 +18,9 @@ from .api import (ConnectError, HTTPStatusError, dataapi, integration_genesis,
                   statapi)
 from .api.duckling import TimeRange
 from .api.integration_genesis.schemas import SensorMetadata
-from .common import (ClientException, JSONCustomEncoder, ServerException,
+from .common import (ClientException, ServerException,
                      action_exception_handle_graceful)
-from .language_helper import summary_AggregationOut, user_to_timeperiod, string_timestamp_to_human
+from .language_helper import user_to_timeperiod, string_timestamp_to_human
 
 LOGGER = logging.getLogger(__name__)
 
@@ -177,77 +176,6 @@ class ActionSensorDataLoad(Action):
         # TODO: Save data into context memory
         # --- Store dataframe in memory with ID. If user requests different dataset, add new ID.
         # --- Save ID in context memory, keep using it from there.
-        # TODO: Do analysis (outliers, missing, etc.) when data is loaded ONLY.
-
-        if response_data is None:
-            dispatcher.utter_message("Sorry, data for {sensor_req} isn't available.".format(
-                sensor_req=user_input.get('user_req_metric')
-            ))
-        else:
-            data, metadata = response_data
-
-            if data.empty:
-                dispatcher.utter_message("Sorry, data for {sensor_type} isn't available for the time range.".format(
-                    sensor_type=metadata['sensor_type']
-                ))
-            else:
-                # Any special things found with the data for further analysis
-                data_insights = []
-
-                # Run aggregation
-                aggregated_result, outliers_result = await asyncio.gather(
-                    statapi.aggregation(data, aggregation),
-                    statapi.outliers(data)
-                )
-
-                agg_response_text = summary_AggregationOut(aggregated_result, unit_symbol=metadata["display_unit"])
-                dispatcher.utter_message(agg_response_text)
-
-                aggregation_followup_response_buttons: List[Dict[str, str]] = []
-
-                # aggregation_followup_response_buttons.extend([
-                #     {"title": "Min", "payload": "minimum"},
-                #     {"title": "Max", "payload": "maximum"},
-                #     {"title": "Average", "payload": "average"},
-                #     {"title": "Current", "payload": "current"}
-                # ])
-
-                if not outliers_result.empty:
-                    dispatcher.utter_message(
-                        "Discovered {outlier_count} outlier value(s) within the data.".format(
-                            outlier_count=outliers_result['value'].count()
-                        )
-                    )
-
-                    # Button to display outlier's info
-                    # aggregation_followup_response_buttons.append({
-                    #     "title": "Outlier details",
-                    #     "payload": "describe the outlier"
-                    # })
-
-                    for _, outlier_ser in outliers_result.iterrows():
-                        data_insights.append({
-                            "type": "outlier",
-                            "data_point": outlier_ser
-                        })
-
-                # TODO: When UI is ready, enable this
-                # dispatcher.utter_message(
-                #     "Additional actions",
-                #     buttons=aggregation_followup_response_buttons
-                # )
-
-                update_statement_context(tracker, events, {
-                    "intent_used": tracker.latest_message.get('intent'),
-                    "action_performed": self.name(),
-                    "extra_data": json.dumps({
-                        "insights": data_insights,
-                        "operation": "aggregation",
-                        "result": aggregated_result
-                    }, cls=JSONCustomEncoder)
-                })
-
-        return events
 
 
 class ActionFetchReport(Action):
@@ -290,45 +218,6 @@ class ActionFetchReport(Action):
         return events
 
 
-class ActionFormMetricData(FormValidationAction):
-    def name(self) -> Text:
-        return "form_metric_data"
-
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-
-        return ["metric", "location"]
-
-    def submit(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-    ) -> List[Dict]:
-        """Define what the form has to do
-            after all required slots are filled"""
-
-        # utter submit template
-        dispatcher.utter_message(template="action_metric_aggregate", metric=tracker.get_slot('metric'),
-                                 location=tracker.get_slot('location'))
-        return []
-
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        """A dictionary to map required slots to
-            - an extracted entity
-            - intent: value pairs
-            - a whole message
-            or a list of them, where a first match will be picked"""
-
-        return {
-            "metric": [self.from_entity(entity="metric", intent='query_metric_aggregate'),
-                       self.from_text()],
-            "location": [self.from_entity(entity="location", intent="query_metric_aggregate"),
-                         self.from_text()],
-        }
-
-
 class ActionShowSensorList(Action):
     def name(self):
         return "action_show_sensor_list"
@@ -354,35 +243,6 @@ class ActionShowSensorList(Action):
             dispatcher.utter_message(text=sensorlist_msg)
 
         return []
-
-
-class ActionGetSensor(Action):
-    def name(self):
-        return "action_search_sensor_by_name"
-
-    @action_exception_handle_graceful
-    async def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
-        events: List[Dict[str, Any]] = []
-        LOGGER.info("Running action_search_sensor_by_name")
-        sensor_name: Optional[str] = tracker.get_slot("sensor_name")
-        LOGGER.info("slot sensor_name filled with %s", sensor_name)
-        try:
-            sensor = await integration_genesis.determine_user_request_sensor(
-                sensor_name=sensor_name,  # TODO: Get from slot
-            )
-            reset_slot(slot_name="metric", value=sensor["sensor_type"], events=events)
-            reset_slot(slot_name="location", value=sensor["sensor_location"]['unit_alias'], events=events)
-            # TODO: [NARAYAN] Improve grammar, make it shorter, show only sensor name and not dict.
-            dispatcher.utter_message(
-                text=f"found sensor as to be : {sensor['sensor_name']} with alias {sensor['sensor_alias']}")
-        except HTTPStatusError as exc:
-            if exc.response.is_client_error:
-                raise ClientException("Requested data does not exist.")
-        except ConnectError as e:
-            raise ServerException("Couldn't connect to Abot backend.", e)
-        except Exception as e:  # TODO: Capture specific exceptions
-            raise ServerException("Something went wrong while looking up sensor data.", e)
-        return events
 
 
 class ActionResetSlot(Action):
@@ -489,7 +349,7 @@ class ActionShowTimerangeValue(Action):
         HUMAN_TIME_FORMAT = "%c"
         time_range = await user_to_timeperiod(tracker, autoset_default=None)
         if time_range:
-            dispatcher.utter_message(text="Data time range: {t_from} till {t_to}".format(
+            dispatcher.utter_message(text="Data time range: *{t_from}* till *{t_to}*".format(
                 t_from=time_range["from"].strftime(HUMAN_TIME_FORMAT),
                 t_to=time_range["to"].strftime(HUMAN_TIME_FORMAT)
             ))
